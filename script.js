83a8f385ea490a7957f0f8215d56ce4d7339e375
egorcikg <guseve822@gmail.com>
2021-10-04 21:51:26 +0400

    qwq

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ let current = 0;
 let mains = document.querySelectorAll(".main");
 let mains_len = mains.length;
 //
+function next() {
+  if(current < mains_len-1) {
+    mains[current].style.display = "none";
+    mains[current+1].style.display = "flex";
+    current++;
+  } else {
+    mains[current].style.display = "none";
+    current = 0;
+    mains[current].style.display = "flex";
+  }
+}
+function prev() {
+  if(current > 0) {
+    mains[current].style.display = "none";
+    mains[current-1].style.display = "flex";
+    current--;
+  } else {
+    mains[current].style.display = "none";
+    current = mains.length-1;
+    mains[current].style.display = "flex";
+  }
+}
 function keydown(event) {
   if(event.code == 'ArrowRight') {
-    if(current < mains_len-1) {
-      mains[current].style.display = "none";
-      mains[current+1].style.display = "flex";
-      current++;
-    } else {
-      mains[current].style.display = "none";
-      current = 0;
-      mains[current].style.display = "flex";
-    }
+    next();
   }
   if(event.code == 'ArrowLeft') {
-    if(current > 0) {
-      mains[current].style.display = "none";
-      mains[current-1].style.display = "flex";
-      current--;
-    } else {
-      mains[current].style.display = "none";
-      current = mains.length-1;
-      mains[current].style.display = "flex";
-    }
+    prev();
   }
 }
 //
 document.addEventListener("keydown",keydown);
+setInterval(next, 3000);
