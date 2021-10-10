07e2092068122b15ed7bbbffd67265d7b44117ee
fidesticor <fidesticor@gmail.com>
2021-10-10 22:56:54 +0300

    ro

--- script.js
+++ script.js
@@ -1,37 +1,97 @@
-let current = 0;
-let mains = document.querySelectorAll(".main");
-let mains_len = mains.length;
+let $main = null;
+let $book = null;
 //
-function next() {
-  if(current < mains_len-1) {
-    mains[current].style.display = "none";
-    mains[current+1].style.display = "flex";
-    current++;
-  } else {
-    mains[current].style.display = "none";
-    current = 0;
-    mains[current].style.display = "flex";
-  }
+let $current = -1;
+let $pages = null;
+let $pageslen = null;
+//
+//
+function create_page_in_huhale(huhale) {
+  let page = document.createElement('div');
+  page.classList.add("page");
+  return(huhale.appendChild(page));
 }
-function prev() {
-  if(current > 0) {
-    mains[current].style.display = "none";
-    mains[current-1].style.display = "flex";
-    current--;
-  } else {
-    mains[current].style.display = "none";
-    current = mains.length-1;
-    mains[current].style.display = "flex";
+//
+function vorosu(halelu,huhale) {
+  let hehuhale = huhale.clientHeight;
+  huhale.style.height = "auto";
+  let te = 0;
+  let tadana = false;
+  let vereru = [];
+  let halelula = halelu.length;
+  while(te<halelula) {
+    let kohale = halelu[te].cloneNode(true);
+    if(tadana) {
+      vereru.push(kohale);
+      te++;
+    } else {
+      kohale = huhale.appendChild(kohale);
+      let nehehuhale = huhale.clientHeight;
+      if(nehehuhale <= hehuhale) {
+        te++;
+      } else {
+        kohale.remove();
+        tadana = true;
+      }
+    }
   }
+  huhale.style.maxHeight = hehuhale+"px";
+  huhale.style.minHeight = hehuhale+"px";
+  //
+  return(vereru);
 }
+//
+function convert_main_to_book() {
+  //
+  $main = document.querySelector(".main");
+  $book = document.querySelector(".book");
+  //
+  let halelu = $main.querySelectorAll(".info");
+  let fesone = true;
+  while(fesone) {
+    let huhale = create_page_in_huhale($book);
+    halelu = vorosu(halelu,huhale);
+    if(typeof halelu=='undefined' || halelu.length==0) {
+      fesone = false;
+    }
+  }
+  //
+  $pages = $book.querySelectorAll(".page");
+  $pageslen = $pages.length;
+}
+//
+//
+function scroll_to(element) {
+  element.scrollIntoView(
+      {
+       behavior:"smooth",
+       block:"end",
+       inline:"nearest"
+      });
+}
+//
+function scroll_to_next_page() {
+  if($current < $pageslen-1) {
+    $current++;
+  } else {
+    $current = 0;
+  }
+  console.log($current);
+  scroll_to($pages[$current]);
+}
+//
 function keydown(event) {
-  if(event.code == 'ArrowRight') {
-    next();
+  if(event.code == 'KeyJ') {
+    scroll_to_next_page();
   }
-  if(event.code == 'ArrowLeft') {
-    prev();
-  }
+}
+//
+function load_dom() {
+  convert_main_to_book();
+  scroll_to_next_page();
+  //setInterval(scroll_to_next_page, 15000);
 }
 //
 document.addEventListener("keydown",keydown);
-setInterval(next, 15000);
+//
+document.addEventListener("DOMContentLoaded",load_dom);
