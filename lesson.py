b52413c58c73c38815d374b2c6f9852da3b01e22
fidesticor <fidesticor@gmail.com>
2021-11-23 11:50:55 +0300

    ioijo

--- lesson.py
+++ lesson.py
@@ -168,7 +168,7 @@ def filter_dict_list(info_dict_list,resope):
         ka = 0
         for ve in set(re.keys()):
             #
-            ka+=1 if ru[n][ve] == re[ve] else ka
+            ka=ka+1 if ru[n][ve] == re[ve] else ka
             print(ve)
             print(ka)
             #
